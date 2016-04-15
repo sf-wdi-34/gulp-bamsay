ac0992981443d9bf609b3aeff96ea813f7c2127f
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2016-04-15 12:10:41 -0400

    changes to starter for webpack

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
-// THIS IS SUPPOSED TO BE INCORRECT CODE FYI :)
-$(document).ready(function(){
-    var h1 = bamsay;
-    $(h1).on(click), function(){
-      console.log("Hello");
-    });
+var component = require('./component');
+require('../css/styles.css')
 
-});
+document.body.appendChild(component());
+// document.body.appendChild(wombat());
