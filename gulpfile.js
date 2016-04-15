f814a20740c5b3652cf2f85b85159b28e6a78a34
Becky Beauchamp <beckybeauchamp1@gmail.com>
2016-04-15 12:10:00 -0400

    Adds dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,15 @@
-var gulp = require("gulp")
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+
+//define a task with the name of 'default'
+// and a callback to perform when the task is ran
+gulp.task('default', function() {
+  console.log('I am the default task!');
+});
+
+gulp.task('jshint', function() {
+  return gulp.src('./js/*.js')
+  .pipe(jshint())
+  .pipe(jshint.reporter(stylish));
+});
