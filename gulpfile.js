8eeaf7b27d602b47e97c43a00661fdd25a08cf80
Becky Beauchamp <beckybeauchamp1@gmail.com>
2016-04-17 18:55:12 -0400

    Adds Solution

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+var sass = require("gulp-sass");
+var watch = require('gulp-watch');
+var connect = require('gulp-connect');
 
 //define a task with the name of 'default'
 // and a callback to perform when the task is ran
@@ -13,3 +16,22 @@ gulp.task('jshint', function() {
   .pipe(jshint())
   .pipe(jshint.reporter(stylish));
 });
+
+gulp.task('sass', function () {
+  return gulp.src('./css/**/*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./css/'))
+  .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./css/**/*.scss', ['sass']);
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    livereload: true
+  })
+});
+
+gulp.task('default', ['sass', 'connect', 'watch']);
